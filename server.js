403666f9c73eebd306e8fc02622a2e65931419a4
DonNavinS <seneviratnanavin@gmail.com>
2022-01-13 17:43:01 -0500

    Server connects to MongoDB

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
-const express = require("express");
-
-const app = express();
+const { ApolloServer } = require("apollo-server");
+const gql = require("graphql-tag");
+const mongoose = require("mongoose");
+const { connectionString } = require("./server/config");
 const PORT = process.env.PORT || 3001;
 
-app.get("/", (req, res) => {
-  res.json({ a: "a" });
+const typeDefs = gql`
+  type Query {
+    sayHi: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    sayHi: () => "Hello",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs: typeDefs,
+  resolvers: resolvers,
 });
 
-app.listen(PORT, () => {
-  console.log(`SERVER RUNNING ON PORT ${PORT}`);
-});
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    return server.listen(PORT, () => {
+      console.log(`SERVER RUNNING ON PORT ${PORT} AND CONNECTED TO MONGODB`);
+    });
+  });
